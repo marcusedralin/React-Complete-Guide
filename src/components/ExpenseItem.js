26fdcdfceffd4ec46829d22fe7fabe3a4d0ed0e4
Marcus Edralin <medralin36@yahoo.com>
2021-05-16 22:20:16 -0700

    Added Example Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+    return (
+        <div>
+            <div>May 15 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$376.92</div>
+            </div>
+        </div>
+    )
+}
